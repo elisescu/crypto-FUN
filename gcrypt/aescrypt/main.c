8c64b66bb44efaa967d10aea7c6df09c103c97e8
Kyle Isom <coder@kyleisom.net>
2011-01-05 21:40:53 -0700

    Start option parsing.

--- gcrypt/aescrypt/main.c
+++ gcrypt/aescrypt/main.c
@@ -6,14 +6,38 @@
  */
 
 #include "config.h"
+#include "crypto.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+
 #include "cryptinit.h"
 #include "cryptfile.h"
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     char *key;
+    struct *crypto_t aes;           /* stores algorith / key info */
+    char *keyfile   = NULL;         /* file contain key */
+    char *infile    = NULL;         /* input file */
+    char *outfile   = NULL;         /* output file */
+
+    /* allocate memory for crypto struct */
+    aes = (struct crypto_t *) calloc(1, sizeof(struct crypto_t));
+    if (NULL == aes) {
+        fprintf(stderr, "[!] error in crypto_t calloc!\n");
+        exit 2;
+    }
+
+    /* parse  command line options */
+    opterr  = 0;
+    while ((c = getopt(argc, argv, "i:o:edb:k:")) != -1) {
+        case 'i':
+            
+    }
+
     if (crypto_init()) {
         return EXIT_FAILURE;
     }
